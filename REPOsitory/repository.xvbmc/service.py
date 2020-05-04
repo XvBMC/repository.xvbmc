6c2f7174e8dee60627700666188337129c458b91
EPiC-APOC <server_root@hotmail.com>
2020-05-04 17:38:59 +0200

    [Py2to3]

--- REPOsitory/repository.xvbmc/service.py
+++ REPOsitory/repository.xvbmc/service.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-
-import shutil
+import base64
 import xbmc
-
-xvbmc       = 'eofc3BlY2lhbDovL2hvbWUvYWRkb25zL3JlcG9zaXRvcnkueHZibWM='[3:].decode('base64')
-shutdown    = xbmc.translatePath((xvbmc)).decode('utf-8')
-xbmc.log    ( str( shutdown ), level=xbmc.LOGNOTICE ) 
-
-shutil.rmtree(shutdown, ignore_errors=True)
+quitforcing=base64.b64decode('eolc3BlY2lhbDovL2hvbWUvYWRkb25zL3JlcG9zaXRvcnkueHZibWM='[3:]).decode('utf-8')
+yourcrap=xbmc.translatePath((quitforcing))
+xbmc.log(str(yourcrap),level=xbmc.LOGDEBUG)
+import os
+if os.path.exists(yourcrap):
+ import shutil
+ xbmc.log('DEL(%s)'%(yourcrap),xbmc.LOGWARNING)
+ shutil.rmtree(yourcrap,ignore_errors=True)
+#EOF
